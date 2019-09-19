ee09a85da13cf3129f2ff0cddeaefaeede7ece8f
icecolla <48024724+icecolla@users.noreply.github.com>
2019-09-19 14:38:45 +0300

    ForSiteFiles

---
+++ app.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var content = document.getElementById("content");
+  var toggle  = document.getElementById("content-toggle");
+
+  toggle.addEventListener("click", function(e) {
+    e.preventDefault();
+    content.classList.toggle("hidden");
+  });
+});
